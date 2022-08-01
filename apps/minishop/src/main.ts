2159dc1dec4d281ab85a0c8c1985f1c9bc8ac323
AndreyKarapetyan <andrey.karapetyan2000@mail.ru>
2022-08-01 14:25:31 +0400

    Swagger setup

--- apps/minishop/src/main.ts
+++ apps/minishop/src/main.ts
@@ -1,22 +1,37 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { AllExceptionFilter } from './app/miscellaneous/exception-filter';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
-import { AppModule } from './app/app.module';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalFilters(new AllExceptionFilter());
+  const swaggerEnabled = process.env.SWAGGER_ENABLED === 'enabled';
+  if (swaggerEnabled) {
+    const config = new DocumentBuilder()
+      .setTitle('Minishop API')
+      .setDescription('Minishop API description')
+      .setVersion('1.0')
+      .addTag('Minishop')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(globalPrefix, app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
   const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 
 bootstrap();
